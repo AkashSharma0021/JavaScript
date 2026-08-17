ef7d13495cbb44133fd1b93cd4a209bd91815dde
AkashSharma0021 <143161590+AkashSharma0021@users.noreply.github.com>
2026-08-17 16:52:04 +0000

    datatypes

---
+++ 01_basics/01variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553 
+let accountEmail = "aksh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 // not allowed 
+console.log(accountId);
+accountEmail = "abc@gmail.com"
+accountPasword =  "4569"
+accountCity = "delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,24 @@
+"use strict "  // treat as a newer version of javascript
+// alert(3+3) // we get error as we are using nlde.js and in browser we get 6
+// make sure code is readable 
+console.log(3+3)
+console.log("akash")
+
+
+let name = "Akash"
+let age = 18
+let isLoggedIn = false
+
+// number=>2 to power 53
+//bigint
+
+//string => either using "" or also ' '
+// boolean => true or false
+// null => standalone value
+let state = null // empty
+// undefined => value is not assigned 
+// symbol => unique
+
+
+// object
+console.log(typeof "akash")
