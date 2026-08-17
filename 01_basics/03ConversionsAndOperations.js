8acdb7607c666c88a8692630d5d818ea7b530275
AkashSharma0021 <143161590+AkashSharma0021@users.noreply.github.com>
2026-08-17 17:30:38 +0000

    operations

--- 01_basics/03ConversionsAndOperations.js
+++ 01_basics/03ConversionsAndOperations.js
@@ -1,5 +1,5 @@
 let score = '33abc'
-
+/*
 console.log(typeof score);
 console.log(typeof(score));
 
@@ -21,3 +21,36 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber );
+
+// ************************************ Operations *******************************
+
+let value = 3;
+let negvalue = -value
+console.log(negvalue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2); // power of two
+*/
+
+let str1 = "hello";
+let str2 = " akash";
+let str3 = str1 + str2;
+console.log(str3);
+console.log(1+"2")
+console.log("1" + 2)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+console.log(+"");
+
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+gameCounter++;  // ++gameCounter(prefix)
+console.log(gameCounter);
