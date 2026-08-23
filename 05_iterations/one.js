8a40c79026e168b40e3ea950033ff9353c05ae7e
AkashSharma0021 <143161590+AkashSharma0021@users.noreply.github.com>
2026-08-23 09:53:22 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,36 @@
+// for loop
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// for(let i = 1;i<10;i++){
+//     console.log(`The table of ${i} :`)
+//     for(let j=1;j<=10;j++){
+//         console.log(i+'*'+j+'='+ i*j)
+//     }
+// }
+
+// let myarr = ["akash","akash1","akash2"]
+// for(let i = 0; i<myarr.length;i++){
+//     console.log(myarr[i]);
+// }
+
+//****keywords********* */
+// break and continue
+
+// for(let i = 1;i<10;i++){
+//    if(i == 5){
+//     console.log("detected 5")
+//     break ;
+//    }
+//    console.log(`the value of i is ${i}`)
+// }
+
+
+for(let i = 1;i<10;i++){
+   if(i == 5){
+    console.log("detected 5")
+    continue ;
+   }
+   console.log(`the value of i is ${i}`)
+}
